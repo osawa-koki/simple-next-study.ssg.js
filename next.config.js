9e75143ac50caad191ef63474864f028a9663fd7
osawa-koki <github@osawa.dev>
2022-12-08 21:27:16 +0900

    ルートパス指定した。

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/* 公開時のサブディレクトリ */
+const SUB_DIRECTORY = "/sub_directory";
+
+/* 本番環境と開発環境の分岐用のフラグ */
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  // 本番環境では SUB_DIRECTORY それ以外は空欄
+  basePath: isProd ? SUB_DIRECTORY : "",
+};

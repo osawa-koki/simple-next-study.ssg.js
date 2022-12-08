c39244b17c4e8c423ba0b7f26f81ed867f204f94
osawa-koki <github@osawa.dev>
2022-12-08 21:28:39 +0900

    小さな修正。

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /* 公開時のサブディレクトリ */
-const SUB_DIRECTORY = "/sub_directory";
+const SUB_DIRECTORY = "/simple-next-study.ssg.js";
 
 /* 本番環境と開発環境の分岐用のフラグ */
 const isProd = process.env.NODE_ENV === "production";

e4f0e0259c67967f3d4816d1b3a864214b696bc9
osawa-koki <github@osawa.dev>
2023-01-01 20:33:49 +0900

    小さな修正。

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,9 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <>
+      <div className='Central'>
+        <img src="tako.png" alt="Logo" />
+      </div>
     </>
   );
 };

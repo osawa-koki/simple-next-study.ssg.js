32e85b872a201aef80d4aac9c9c54836ba09c987
osawa-koki <github@osawa.dev>
2023-01-01 21:34:56 +0900

    フォーム。

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import '../styles/styles.scss';
 
 import '../styles/index.scss';
+import '../styles/about.scss';
+import '../styles/contact.scss';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
